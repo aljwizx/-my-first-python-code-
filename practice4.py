1ef19f9e2472a6e18226367d2a91d2ce166755fc
Alla <allaahtamova53@icloud.com>
2026-09-04 21:55:57 +0500

    uptade practice4 code

---
+++ practice4.py
@@ -0,0 +1,6 @@
+workouts = [30, 45, 20, 50, 40]
+for minutes in workouts:
+    if minutes >= 30:
+        print(f"{minutes} мин - Отличная тренировка!")
+    else:
+        print(f"{minutes} мин - Маловато, но тоже шаг вперед!")
