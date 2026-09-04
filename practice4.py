773e474c04959d55904f0741bf972c2bc36f7d6a
Alla <allaahtamova53@icloud.com>
2026-09-04 22:01:11 +0500

    add stats

--- practice4.py
+++ practice4.py
@@ -4,3 +4,11 @@ for minutes in workouts:
         print(f"{minutes} мин - Отличная тренировка!")
     else:
         print(f"{minutes} мин - Маловато, но тоже шаг вперед!")
+
+
+workouts = [30, 45, 20, 50, 40]
+total_minutes = sum(workouts)
+total_days = len(workouts)
+average = total_minutes / total_days
+print(f"Всего отработано: {total_minutes} минут")
+print(f"В среднем за день: {average} минут")
